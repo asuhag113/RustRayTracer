39b57b63d3029182351815f89af8527d4000d25a
Abhijay Suhag <abhijay113@gmail.com>
2023-10-14 02:44:37 -0400

    simplify sphere intersection code

--- src/main.rs
+++ src/main.rs
@@ -103,11 +103,11 @@ fn hit_sphere(center: &Point3D, radius: f32, ray: &Ray) -> f32 {
     // note, for now there is an intentional bug where the camera+scene cannot tell if the sphere is
     // in front of the camera (-z) or behind the camera (+z), so a sphere with z +1 and -1 will look the same
     let oc = ray.origin() - *center;
-    let a = ray.direction().dot(ray.direction());
-    let b = 2.0 * oc.dot(ray.direction());
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    return if discriminant < 0.0 { -1.0 } else { (-b - f32::sqrt(discriminant)) / (2.0 * a) };
+    let a = ray.direction().length_squared();
+    let half_b = oc.dot(ray.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    return if discriminant < 0.0 { -1.0 } else { (-half_b - f32::sqrt(discriminant)) / a };
 }
 
 fn ray_color(ray: &Ray) -> Color {
