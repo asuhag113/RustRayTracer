a0bfd5674e55121ee5026dd56bf50199bf199d80
Abhijay Suhag <abhijay113@gmail.com>
2023-10-15 03:07:52 -0400

    add clamp to interval

--- src/interval.rs
+++ src/interval.rs
@@ -13,6 +13,15 @@ impl Interval {
     pub fn surrounds(&self, x: f32) -> bool {
         return self.min < x && x < self.max;
     }
+    pub fn clamp(&self, x: f32) -> f32 {
+        if x < self.min {
+            return self.min;
+        }
+        if x > self.max {
+            return self.max;
+        }
+        return x;
+    }
 }
 
 pub const EMPTY: Interval = Interval { min: f32::INFINITY, max: f32::NEG_INFINITY };
