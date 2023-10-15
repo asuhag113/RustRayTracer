308be17944ec133b491fdd2d5d3a3f1bed9316ee
Abhijay Suhag <abhijay113@gmail.com>
2023-10-14 20:24:04 -0400

    update sphere to use new hitrecord return format

--- src/sphere.rs
+++ src/sphere.rs
@@ -12,13 +12,7 @@ impl Sphere {
 }
 
 impl Hittable for Sphere {
-    fn hit(
-        &self,
-        ray: &crate::ray::Ray,
-        ray_tmin: f32,
-        ray_tmax: f32,
-        hit_record: &mut HitRecord
-    ) -> bool {
+    fn hit(&self, ray: &crate::ray::Ray, ray_tmin: f32, ray_tmax: f32) -> Option<HitRecord> {
         // formula for ray-sphere intersection
         // note, for now there is an intentional bug where the camera+scene cannot tell if the sphere is
         // in front of the camera (-z) or behind the camera (+z), so a sphere with z +1 and -1 will look the same
@@ -28,7 +22,7 @@ impl Hittable for Sphere {
         let c = oc.length_squared() - self.radius * self.radius;
         let discriminant = half_b * half_b - a * c;
         if discriminant < 0.0 {
-            return false;
+            return None;
         }
         let sqrtd = f32::sqrt(discriminant);
 
@@ -37,15 +31,25 @@ impl Hittable for Sphere {
         if root <= ray_tmin || ray_tmax <= root {
             root = (-half_b + sqrtd) / a;
             if root <= ray_tmin || ray_tmax <= root {
-                return false;
+                return None;
             }
         }
-
-        hit_record.t = root;
-        hit_record.p = ray.at(hit_record.t);
-        let outward_normal = (hit_record.p - self.center) / self.radius;
-        hit_record.set_face_normal(ray, &outward_normal);
-
-        return true;
+        let p = ray.at(root);
+        // our normals always point against the ray, so we must store which side of the surface the ray is on
+        // an alternative to this would be to determine the side of the surface during coloring
+        let outward_normal = (p - self.center) / self.radius;
+        let front_face = ray.direction().dot(outward_normal) < 0.0;
+        return Some(
+            HitRecord::new(
+                p,
+                if front_face {
+                    outward_normal
+                } else {
+                    -outward_normal
+                },
+                root,
+                front_face
+            )
+        );
     }
 }
